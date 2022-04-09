aa632214554f9397e269ebf7eb1299e4ef979591
혜린 <98096740+sunnylina@users.noreply.github.com>
2022-04-09 19:13:40 +0900

    Add files via upload

---
+++ 03장 print()문.py
@@ -0,0 +1,37 @@
+print("ABCDE FGHIJ")
+print('ABCDE FGHIJ')
+print("I'm ok!")
+print('"ABCDE FGHIJ"')
+print("""ABCDE FGHIJ,
+
+KLMNO PQRST""")
+print('''ABCDE FGHIJ,\
+KLMNO PQRST''')
+
+print('ABCDE', 'FGHIJ');
+print(10, 20)
+
+print('ABCDE' + 'FGHIJ')
+print(10 + 20)
+
+print('ABCDE' * 3)
+print(10 * 3)
+
+print('ABCDE', end = ',')
+print('GHI' , 'JKL', sep = ',')
+
+"""
+블럭 주석 공간으로 프로그램 실행시 어떠한 영향도 받지 않는다.
+program by 오일러
+"""
+
+'''
+Block comment space is not affected by any program execution.
+program by euler
+'''
+
+print("""#
+##
+###
+####
+#####""")
